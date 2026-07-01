c7eef63275c29de18ce6bd172d7ac877b59e93b4
Nitish2004k <nitishbaluakumar@gmail.com>
2026-07-01 23:57:25 +0530

    Fix destroyLisitng typo to destroyListing

--- controllers/listings.js
+++ controllers/listings.js
@@ -8,7 +8,7 @@ module.exports.index = (async (req, res) => {
 
 
 module.exports.renderNewForm = (req, res) => {
-    res. render("listings/new.ejs");
+    res.render("listings/new.ejs");
 };
 
 
@@ -24,8 +24,8 @@ module.exports.createListing = async (req, res) => {
     let filename = req.file.filename;
 
     const newListing = new Listing(req.body.listing);
-    newListing.owner= req.user_id;
-    newListing.image = {url, filename};
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
 
     await newListing.save();
     req.flash("success", "new listing created!");
@@ -44,12 +44,78 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect("/listings"); // ✅ res. add kiya
+    res.redirect("/listings");
 };
 
-module.exports.destroyLisitng = async (req, res) => {
+module.exports.destroyListing = async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
 };
+
+
+
+
+
+
+
+
+
+
+
+// const Listing = require("../model/listing")
+
+
+// module.exports.index = (async (req, res) => {
+//     const allListings = await Listing.find({});
+//     res.render("listings/index.ejs", { allListings });
+// });
+
+
+// module.exports.renderNewForm = (req, res) => {
+//     res. render("listings/new.ejs");
+// };
+
+
+
+// module.exports.showListing = async (req, res) => {
+//     let { id } = req.params;
+//     const listing = await Listing.findById(id);
+//     res.render("listings/show.ejs", { listing });
+// };
+
+// module.exports.createListing = async (req, res) => {
+//     let url = req.file.path;
+//     let filename = req.file.filename;
+
+//     const newListing = new Listing(req.body.listing);
+//     newListing.owner= req.user_id;
+//     newListing.image = {url, filename};
+
+//     await newListing.save();
+//     req.flash("success", "new listing created!");
+//     res.redirect("/listings");
+// }
+
+
+
+// module.exports.renderEditForm = async (req, res) => {
+//     let { id } = req.params;
+//     const listing = await Listing.findById(id);
+//     res.render("listings/edit.ejs", { listing });
+// };
+
+
+// module.exports.updateListing = async (req, res) => {
+//     let { id } = req.params;
+//     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+//     res.redirect("/listings"); // ✅ res. add kiya
+// };
+
+// module.exports.destroyLisitng = async (req, res) => {
+//     let { id } = req.params;
+//     let deletedListing = await Listing.findByIdAndDelete(id);
+//     console.log(deletedListing);
+//     res.redirect("/listings");
+// };
